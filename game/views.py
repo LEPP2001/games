b2427c6e8aac4ca3c701de9796a31fd974685215
LuisEPP <96139261+LEPP2001@users.noreply.github.com>
2023-05-23 13:12:30 -0600

    Update views.py

--- game/views.py
+++ game/views.py
@@ -22,33 +22,33 @@ class MoveViewSet(viewsets.ModelViewSet):
     serializer_class = MoveSerializer
 
 # Renderizar la base
-@login_required
+# @login_required
 def base(request):
     return render(request, 'base.html')
 
 # Renderizar la vista del perfil
-@login_required
+# @login_required
 def profile(request):
     return render(request, 'profile.html')
 
 # Renderizar los juegos
-@login_required
+# @login_required
 def games(request):
     return render(request, 'games.html')
 
 # Renderizar el aboutme
-@login_required
+# @login_required
 def about(request):
     return render(request, 'about.html')
 
 # Renderizar la salida
-@login_required
+# @login_required
 def exit(request):
     logout(request)
     return redirect(base)
 
 # Renderizar html con el juego gato
-@login_required
+# @login_required
 def gato(request):
     url = 'https://juegosgamesx.blob.core.windows.net/gato/gato.html'
     response = requests.get(url)
@@ -56,7 +56,7 @@ def gato(request):
     return render(request, 'games/gato/gato.html', {'content': content})
 
 # Renderizar html con las instrucciones del gato
-@login_required
+# @login_required
 def gatoInst(request):
     url = 'https://juegosgamesx.blob.core.windows.net/gato/instruccionesGato.html'
     response = requests.get(url)
@@ -64,7 +64,7 @@ def gatoInst(request):
     return render(request, 'games/gato/gatoInst.html', {'content': content})
 
 # Renderizar html con el juego T. Hanoi
-@login_required
+# @login_required
 def hanoi(request):
     url = 'https://juegosgamesx.blob.core.windows.net/torres-hanoi/torresHanoi.html'
     response = requests.get(url)
@@ -72,7 +72,7 @@ def hanoi(request):
     return render(request, 'games/hanoi/hanoi.html', {'content': content})
 
 # Renderizar html con las instrucciones de las T. Hanoi
-@login_required
+# @login_required
 def hanoiInst(request):
     url = 'https://juegosgamesx.blob.core.windows.net/torres-hanoi/instruccionesHanoi.html'
     response = requests.get(url)
