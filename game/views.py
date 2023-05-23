ea34e957c3cb930bc72381ca9d32b01e1727bc24
LuisEPP <96139261+LEPP2001@users.noreply.github.com>
2023-05-23 01:21:06 -0600

    Update views.py

--- game/views.py
+++ game/views.py
@@ -8,6 +8,7 @@ from .serializers import UserSerializer, GameSerializer, MoveSerializer
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login
 import requests
+from django.views.decorators.csrf import csrf_protect, get_token
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -23,6 +24,8 @@ class MoveViewSet(viewsets.ModelViewSet):
 
 # Renderizar la base
 @login_required
+@csrf_protect
+@ensure_csrf_cookie
 def base(request):
     return render(request, 'base.html')
 
@@ -80,6 +83,8 @@ def hanoiInst(request):
     return render(request, 'games/hanoi/hanoiInst.html', {'content': content})
 
 # Renderizar el registro de usuario
+@csrf_protect
+@ensure_csrf_cookie
 def register(request):
     data = {
         'form' : CustomUserCreationForm()
