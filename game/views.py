a2e8f5d4166e8a63cdafddcf8c183f4ca32ff66b
LuisEPP <96139261+LEPP2001@users.noreply.github.com>
2023-05-23 01:56:39 -0600

    Update views.py

--- game/views.py
+++ game/views.py
@@ -24,8 +24,6 @@ class MoveViewSet(viewsets.ModelViewSet):
 
 # Renderizar la base
 @login_required
-@csrf_protect
-@ensure_csrf_cookie
 def base(request):
     return render(request, 'base.html')
 
